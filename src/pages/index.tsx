a0b5bad313497fb989938424a4c71054414addd3
ilovethedrama <bluemagicuk@yahoo.co.uk>
2023-04-04 00:52:41 +0100

    Pre-commit test and eslint config update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,11 @@ import styles                           from "@/styles/Home.module.css"
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  function doRaeMi(): number {
+
+    return 33;
+  }
   return (
     <>
       <Head>
